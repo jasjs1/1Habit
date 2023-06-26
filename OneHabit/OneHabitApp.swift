9ecccef4a9c8a14665429eb242bf94653256c836
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-06-25 21:08:54 -0500

    change label for content view

--- OneHabit/OneHabitApp.swift
+++ OneHabit/OneHabitApp.swift
@@ -6,15 +6,27 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
 
 @main
 struct OneHabitApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             TabView {
                 ContentView()
                     .tabItem {
-                        Label("Habits", systemImage: "calendar")
+                        Label("Your Habits", systemImage: "calendar")
                     }
                 CreateHabit()
                     .tabItem {
