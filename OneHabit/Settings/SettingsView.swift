3aeb1a9a0db7e543aa5b8586748c86327a385bf4
Jayce Sagvold <97198910+jasjs1@users.noreply.github.com>
2023-06-28 15:43:16 -0500

    first commit

---
+++ OneHabit/Settings/SettingsView.swift
@@ -0,0 +1,67 @@
+import SwiftUI
+
+//struct Habit: Identifiable, Codable {
+//    var id = UUID()
+//    var name: String
+//    var selectedDays: [String]
+//}
+
+struct SettingsView: View {
+    @State private var name: String = "Enter your name here"
+    
+    var body: some View {
+        NavigationView {
+            List {
+                HStack(alignment: .center) {
+                    Spacer()
+                    VStack {
+                        Image(systemName: "person.circle.fill")
+                            .font(.system(size: 110))
+                            .foregroundColor(.secondary)
+                        
+                        TextField("Enter your name", text: $name)
+                            .multilineTextAlignment(.center) // Center align the TextField text
+                            .frame(maxWidth: .infinity) // Ensure TextField occupies entire available width
+                    }
+                    Spacer()
+                }
+                
+                Section(header: Text("Habit resources")) {
+                    NavigationLink(destination: HabitInformation()) {
+                        Text("Habit Information")
+                    }
+                    
+                    
+                    
+                    Section(header: Text("Resources & support")) {
+                        NavigationLink(destination: CreateAccountView()) {
+                            Text("Create Account")
+                        }
+                        NavigationLink(destination: ContactDeveloperView()) {
+                            Text("Contact Developer")
+                        }
+                    }
+                }
+                .listStyle(GroupedListStyle())
+                .navigationTitle("Settings")
+            }
+        }
+    }
+    
+    
+    private func deleteHabit(_ habit: Habit) {
+        struct Habit: Identifiable, Codable {
+            var id = UUID()
+            var name: String
+            var selectedDays: [String]
+            
+        }
+        
+        
+        struct SettingsView_Previews: PreviewProvider {
+            static var previews: some View {
+                SettingsView()
+            }
+        }
+    }
+}
